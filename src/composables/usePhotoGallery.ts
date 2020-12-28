0bd6dbb2924c7f29cc56e3c51d3ba29da8cbfab0
Atul Kumar <mycodingx@gmail.com>
2020-12-28 19:04:45 +0530

    web app completed

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -1,8 +1,43 @@
 import { ref, onMounted, watch } from 'vue';
 import { Plugins, CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from '@capacitor/core';
 
+export interface Photo{
+    filepath: string;
+    webviewPath?: string
+}
+
 export function usePhotoGallery(){
-    const { Camera } = Plugins;
+    const PHOTO_STORAGE = "photos";
+    const { Camera, Filesystem, Storage } = Plugins;
+    const photos = ref<Photo[]>([]);
+
+    const cachePhotos = () => {
+        Storage.set({
+            key: PHOTO_STORAGE,
+            value: JSON.stringify(photos.value)
+        });
+    };
+
+    watch(photos, cachePhotos);
+
+
+    const loadSaved = async () => {
+        const photoList = await Storage.get({key: PHOTO_STORAGE});
+        const photosInStorage = photoList.value ? JSON.parse(photoList.value) : [];
+
+        for(const photo of photosInStorage){
+            const file = await Filesystem.readFile({
+                path: photo.filepath,
+                directory: FilesystemDirectory.Data
+            });
+
+            photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+        }
+
+        photos.value = photosInStorage;
+    };
+
+    onMounted(loadSaved);
 
     const takePhoto = async () => {
         const cameraPhoto = await Camera.getPhoto({
@@ -10,9 +45,42 @@ export function usePhotoGallery(){
             source: CameraSource.Camera,
             quality: 100
         });
+
+        const fileName = new Date().getTime() + '.jpeg';
+        const savedFileImage = await savePicture(cameraPhoto, fileName);
+        photos.value = [savedFileImage, ...photos.value];
     };
 
+    const convertBlobtoBase64 = (blob: Blob) => new Promise((resolve, reject)=>{
+        const reader = new FileReader;
+        reader.onerror = reject;
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+    });
+
+    const savePicture = async(photo: CameraPhoto, fileName: string): Promise<Photo> => {
+        let base64Data: string;
+
+        const response = await fetch(photo.webPath!);
+        const blob = await response.blob();
+        base64Data = await convertBlobtoBase64(blob) as string;
+
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: FilesystemDirectory.Data
+        });
+
+        return {
+            filepath: fileName,
+            webviewPath: photo.webPath
+        }
+    }
+
     return{
+        photos,
         takePhoto
     }
 }
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -6,6 +6,13 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
           <ion-fab-button @click="takePhoto()">
             <ion-icon :icon="camera"></ion-icon>
@@ -17,17 +24,17 @@
 
 <script lang="ts">
 import { camera, trash, close } from 'ionicons/icons';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
-import { usePhotoGallery } from '@/composables/usePhotoGallery';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonGrid, IonRow, IonCol, IonImg, IonIcon } from '@ionic/vue';
+import { usePhotoGallery, Photo } from '@/composables/usePhotoGallery';
 
 export default  {
   name: 'Tab2',
-  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonGrid, IonRow, IonCol, IonImg },
+  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonGrid, IonRow, IonCol, IonImg, IonIcon },
 
   setup(){
-    const { takePhoto } = usePhotoGallery();
+    const { takePhoto, photos } = usePhotoGallery();
   return {
-    camera, trash, close, takePhoto
+    camera, trash, close, takePhoto, photos
   }
 }
 }
